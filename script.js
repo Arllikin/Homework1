4822d0a712a8768d34480949a0c6a363def690b6
Ivan <maygetar@gmail.com>
2021-07-28 17:45:33 +0300

    Length of output

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
 
-const numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
+
+do {
+numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?");
+}
+while (numberOfFilms.length > 0 && numberOfFilms.length < 51);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,8 +16,14 @@ const personalMovieDB = {
 }
 
 for (let i = 0; i<2; i++){
+    do {
     const a = prompt("Один из последних просмотренных фильмов?");
+    }
+    while (a.length > 0 && a.length < 51);
+    do {
     personalMovieDB.movies[a] = prompt("На сколько оцените его?");
+    }
+    while (personalMovieDB.movies[a].length > 0 && personalMovieDB.movies[a].length < 51);
 }
 
 // const a = prompt("Один из последних просмотренных фильмов?"),
